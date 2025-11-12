6268ea921b1c8b842715191e93c6cc015e9e4952
George A <agg0073@gmail.com>
2025-11-12 23:30:17 +0400

    smth new

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
+from pydantic import BaseModel
+
+
+class Student(BaseModel):
+    name: str
+    age: str
+
+
 
 LANGUAGES = {
     1: "C++",
@@ -19,8 +27,26 @@ async def helth():
 async def lang(item_id):
     return {"mes": LANGUAGES.get(int(item_id), "js")}
 
+@app.get("/add")
+async def add(a:int, b:int):
+    return {"sum": a + b}
 
+@app.post("/add_student")
+async def add_sudent(model: Student):
+    print(model.model_dump())
+    return model.model_dump()
 
+@app.get("/exam_resp")
+async def add_resp(resp: Response):
+    resp.headers["X-Custom-REsponse-Header"] = "fastapi"
+    
+    return {"mes: ""{fastapi}"}
+
+@app.get("/exam_req")
+async def add_req(req: Request):
+    print(req.headers)
+    
+    return {"mes: ""{fastapi}"}
 
 if __name__ == "__main__":
     uvicorn.run(
